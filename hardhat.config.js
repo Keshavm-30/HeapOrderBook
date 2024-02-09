04ecdc59583718ab34e35163d214f3eaf1e22bf5
keshavm-30 <keshavmg97@gmail.com>
2024-02-09 14:21:26 +0530

    gasReporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 21,
+    enabled: process.env.REPORT_GAS === 'true',
+  },
 };
